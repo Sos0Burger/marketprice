992f391fc208682afd9a3727bb20f7a4766c6141
kek <mail@mail.maillol>
2023-10-13 16:45:23 +0300

    initial commit

---
+++ src/test/java/com/marketprice/marketprice/MarketpriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketprice.marketprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketpriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
